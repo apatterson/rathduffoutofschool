98c7afb6b1f7e5d907e695cb158a5e54c6af9411
Anthony Patterson <anthony.patterson2.0@gmail.com>
2020-03-14 16:18:58 +0000

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -18,7 +18,7 @@ def calculate_order_amount(items):
     # Replace this constant with a calculation of the order's amount
     # Calculate the order total on the server to prevent
     # people from directly manipulating the amount on the client
-    return 10
+    return items[0] * 1.14 + .25
 
 @app.route("/")
 def hello():
