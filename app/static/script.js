9fc9f7b368ce299fd878d95b5a41823032dd21bd
Anthony Patterson <anthony.patterson2.0@gmail.com>
2020-03-14 11:32:22 +0000

    Create script.js

---
+++ app/static/script.js
@@ -0,0 +1,4 @@
+// Set your publishable key: remember to change this to your live publishable key in production
+// See your keys here: https://dashboard.stripe.com/account/apikeys
+var stripe = Stripe('pk_test_jeuCS4XcpYXzvmBb7bDe4tX000HzIA70Xq');
+var elements = stripe.elements();
