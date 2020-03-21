7d0c9092dac45ed6cc83c48d088ca7a7b9729f5d
Anthony Patterson <anthony.patterson2.0@gmail.com>
2020-03-21 11:10:20 +0000

    Update script.js

--- app/static/script.js
+++ app/static/script.js
@@ -21,7 +21,7 @@ fetch("/create-payment-intent", {
   body: JSON.stringify(orderData)
 })
   .then(function(result) {
-    window.location.href = '/' ;
+    return result.json();
   })
   .then(function(data) {
     return setupElements(data);
@@ -100,15 +100,8 @@ var pay = function(stripe, card, clientSecret) {
 var orderComplete = function(clientSecret) {
   // Just for the purpose of the sample, show the PaymentIntent response object
   stripe.retrievePaymentIntent(clientSecret).then(function(result) {
-    var paymentIntent = result.paymentIntent;
-    var paymentIntentJson = JSON.stringify(paymentIntent, null, 2);
-
-    document.querySelector(".sr-payment-form").classList.add("hidden");
-    document.querySelector("pre").textContent = paymentIntentJson;
-
-    document.querySelector(".sr-result").classList.remove("hidden");
     setTimeout(function() {
-      document.querySelector(".sr-result").classList.add("expand");
+      window.location.href = "/?message=Thank you, payment accepted";
     }, 200);
 
     changeLoadingState(false);
